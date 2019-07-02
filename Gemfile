c730e5a3bdaa15b0ec377251f88ced0a16c9e756
jiujianz <cocobskg@gmail.com>
2019-07-02 22:05:53 +0900

    tweet image

--- Gemfile
+++ Gemfile
@@ -65,4 +65,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
-
+gem 'carrierwave'
+gem 'mini_magick'
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,15 @@ class TweetsController < ApplicationController
 
   def index
     @tweet = Tweet.new
+    # @tweets = @user.tweets.includes(:user)
     @tweet_all = Tweet.all
   end
+
+  def create
+    @tweet = @user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    end
+  end
+    
 end
